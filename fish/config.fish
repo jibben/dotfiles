ee12cd3382315b6b033ea42115e7221358a947d5
jibben <jibben@cue.app>
2020-03-09 09:48:33 -0700

    update fish stuff

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,9 @@
 set -x PATH $PATH $GOROOT/bin $GOPATH/bin
 set -x PATH $PATH $CUE_CLIENT_SRC
+set -x PATH $PATH /usr/local/opt/ruby/bin
+set -x PATH $PATH /usr/local/opt/libpq/bin
 
 source /Users/jibben/google-cloud-sdk/path.fish.inc
+rvm default
+
+alias cueobj='go run $CUE_BACKEND_SRC/playground/printobject/printobject.go'
