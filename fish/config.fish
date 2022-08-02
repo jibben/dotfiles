71e9745ff324c1a550a3634d70b90c9b9d2a3663
Jibben Hillen <jibben@stripe.com>
2022-08-02 12:06:39 -0400

    clean up cue stuff from fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,4 @@
 set -x PATH $PATH $GOROOT/bin $GOPATH/bin
-set -x PATH $PATH $CUE_CLIENT_SRC
 set -x PATH $PATH /usr/local/opt/ruby/bin
 set -x PATH $PATH /usr/local/opt/libpq/bin
-
-source /Users/jibben/google-cloud-sdk/path.fish.inc
-rvm default
-
-alias cueobj='go run $CUE_BACKEND_SRC/playground/printobject/printobject.go'
+set -x PATH $PATH /usr/local/bin
